36e854e539016a084c800d376524eb4097d1dde2
kjbrobocat8 <67644996+kjbrobocat8@users.noreply.github.com>
2021-01-02 20:14:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("YOU FOUND THIS?!?")
+console.error("Error: GET: compiled__state returned " + null)
+console.log("Okay, that error was fake");
+console.log("But hopefully there's not a real error here...")
+console.log("testing for errors...");
+var nXjhenkmlfwinoje = null
+var no=undefined
+var what="what????"+undefined
+console.warn("err.."+what+no+nXjhenkmlfwinoje)
